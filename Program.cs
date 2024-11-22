307f5f4c8641f33388f98336a2a2da2125f90561
UPN <UPN@ULCLAB429>
2024-11-22 11:29:35 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chau_fundamentos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese numero de #filas: ");
+            int f= int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese #columnas: ");
+            int c= int.Parse(Console.ReadLine());
+
+            int [,] matriz = new int[f,c];
+            Random rnd = new Random();
+
+            for (int i = 0; i <matriz.GetLength(0);i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    matriz[i,j] = rnd.Next(30,51);
+                    Console.Write(matriz[i, j]+"\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
